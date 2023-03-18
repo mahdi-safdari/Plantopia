087d150d6ec573e7c09371e7c3e7fd9c2adf00ce
Mehdi Safdari <mti.safdari@gmail.com>
2023-03-18 08:59:21 +0330

    created constants file

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color primaryColor = Color(0xFF296E48);
+  static const Color blackColor = Colors.black54;
+  static const String titleOne = "گیاهان را بهتر از قبل درک کن";
+  static const String descriptionOne = "در مورد نگه داری گل و گیاهان میتوانی اطلاعات کسب کنی";
+  static const String titleTwo = "با گیاهان جدید آشنا شو";
+  static const String descriptionTwo = "رز مشکی یا گل رز دوست داری؟ اینجا میتونی پیداش کنی";
+  static const String titleThree = "با یک گل بهار نمیشود، گل بکار";
+  static const String descriptionThree = "هر گلی نیاز داشته باشید در این اپلیکیشن پیدا میکنید";
+}
